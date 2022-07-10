a9ec8c4a27bc95b2a8187cb6d85db8d698349f2c
happym92 <1iamme.00@gmail.com>
2022-07-11 00:08:05 +0900

    log

--- src/main/java/hm/offer/common/controller/CommonController.java
+++ src/main/java/hm/offer/common/controller/CommonController.java
@@ -3,11 +3,16 @@ package hm.offer.common.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
 @RestController
 public class CommonController {
 
 	@GetMapping("/")
 	public String testPage() {
+		log.info("¿¿¥‰!");
 		return "hello";
 	}
 }
