3281e910bc1528f6e65f768be105d64dcd555b0d
happym92 <1iamme.00@gmail.com>
2022-07-11 17:12:25 +0900

    vue

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,8 +4,8 @@
 // })
 module.exports = {  
   outputDir: "../src/main/resources/static/vue",             // outputDir은 npm run build로 빌드 시 파일이 생성되는 위치
-  indexPath: "../index.html",                     // indexPath는 index.html 파일이 생성될 위치를 지정
-  devServer: {                                           // devServer는 Back-End , 즉 Spring Boot의 내장 was의 주소를 작성하게 되면 된다.
+  indexPath: "../index.html",                                // indexPath는 index.html 파일이 생성될 위치를 지정
+  devServer: {                                               // devServer는 Back-End , 즉 Spring Boot의 내장 was의 주소를 작성하게 되면 된다.
     port: 8091,
     proxy: "http://localhost:8090"  
   },  
